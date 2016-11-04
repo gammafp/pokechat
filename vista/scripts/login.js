930d19d6bc4648b519dd0f39f0a8e33e93567bb5
gammafp <gammafp@gmail.com>
2016-11-04 21:55:47 +0100

    Nuevo Proyecto
    Este es la parte que tengo implementada

---
+++ vista/scripts/login.js
@@ -0,0 +1,23 @@
+(function() {
+    var app = angular.module("miApp", ["ngMaterial"]);
+    app.controller("login", login);
+
+    login.$inject = ["$scope"];
+    function login($scope) {
+        var suitch = true;
+        $scope.tipoRegistro = "Registrate";
+        $scope.botonRegistro = function() {
+            if(suitch) {
+                suitch = false;
+                $scope.mostrarRegistro={'margin-top': '0px'};
+                $scope.tipoRegistro = "Login";
+            } else {
+                suitch = true;
+                $scope.mostrarRegistro={'margin-top': '-300px'};
+                $scope.tipoRegistro = "Registrate";
+            }
+
+        }
+
+    }
+})();
