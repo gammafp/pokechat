d92bb512cb4af69a46f77043cde361bd22cc66d0
gammafp <gammafp@gmail.com>
2016-11-12 20:07:58 +0100

    Nuevos cambios
    nuevos cambios

--- vista/scripts/login.js
+++ vista/scripts/login.js
@@ -2,19 +2,17 @@
     var app = angular.module("miApp", ["ngMaterial", "ngMessages", "angular-jwt", "angular-storage", "ngRoute"]);
     var baseUrl = "http://localhost/";
 
-    // Sección de configuraciones
-    app.config(["$routeProvider", "$httpProvider", "jwtInterceptorProvider", "jwtOptionsProvider", function($routeProvider, $httpProvider, jwtInterceptorProvider, jwtOptionsProvider) {
-        jwtOptionsProvider.config({
-            whiteListedDomains: 'http://localhost'
-        });
+// Sección de configuraciones
+    app.config(["$routeProvider", "$httpProvider", "jwtInterceptorProvider", "jwtOptionsProvider", function ($routeProvider, $httpProvider, jwtInterceptorProvider, jwtOptionsProvider) {
+        jwtOptionsProvider.config({ whiteListedDomains: 'http://localhost' });
         $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-        jwtInterceptorProvider.tokenGetter = function() {
+        jwtInterceptorProvider.tokenGetter = function () {
             return localStorage.getItem('token');
         };
         $httpProvider.interceptors.push('jwtInterceptor');
     }]);
 
-    app.run(["$rootScope", "jwtHelper", "store", function($rootScope, jwtHelper, store) {
+    app.run(["$rootScope","jwtHelper", "store", function($rootScope, jwtHelper, store) {
 
         var token = store.get('token') || null;
         var bool = (token) ? jwtHelper.isTokenExpired(token) : token;
@@ -23,15 +21,15 @@
             if (bool) {
                 store.remove("token");
             } else {
-                window.location = baseUrl + "pokechat.html";
+                window.location = baseUrl+"pokechat.html";
             }
         }
     }]);
 
     // esto comprobará si está todo correcto en el front-end
-    app.factory("comprobarAuth", function() {
+    app.factory("comprobarAuth", function () {
         return {
-            comprobar: function(dato) {
+            comprobar: function (dato) {
                 var patron = /^\w+$/;
                 if (!patron.test(dato)) {
                     return false;
@@ -42,13 +40,14 @@
             }
         }
     });
-    // Fin de la sección de configuraciones
+// Fin de la sección de configuraciones
 
     app.controller("login", login);
 
     login.$inject = ["$scope", "$http", "comprobarAuth", "store"];
-
     function login($scope, $http, comprobarAuth, store) {
+
+        // $scope.Login.email.$setValidity('required', false);
         // Logica del login
         // Alerta login
         $scope.loginC = {
@@ -61,7 +60,7 @@
             mensaje: "cojon"
         };
 
-        // Alertas registro
+    // Alertas registro
         $scope.alertaR = {
             mostrar: false,
             mensaje: "Otro entrenador tiene ese nombre de usuario."
@@ -73,36 +72,36 @@
 
         // Esto comprueba si el nombre existe
 
-        // var nombrePillado = true;
-        // $scope.comprobarNombre = function(nombre) {
-        //
-        //     // hacer ajax
-        //     $http({
-        //         method: 'POST',
-        //         url: baseUrl+"controladores/comprobaruser",
-        //         data: {nombre: nombre},
-        //         headers: { 'Content-Type': 'application/json' }
-        //     }).then(function(res) {
-        //
-        //         if(!!res.data) {
-        //             // El usuario existe
-        //             nombrePillado = false;
-        //             $scope.correctoR.mostrar = false;
-        //             $scope.alertaR.mostrar = true;
-        //             $scope.alertaR.mensaje = "Otro entrenador tiene ese nombre de usuario.";
-        //
-        //         } else {
-        //             // Todo correcto y el nombre no existe
-        //             nombrePillado = true;
-        //             $scope.alertaR.mostrar = false;
-        //             $scope.correctoR.mostrar = true;
-        //             $scope.correctoR.mensaje = " Puedes usar ese nombre de usuario."
-        //         }
-        //     });
-        // }
-        //
-        //
-        $scope.LoginFn = function(datos) {
+        var nombrePillado = true;
+        $scope.comprobarNombre = function(nombre) {
+
+            // hacer ajax
+            $http({
+                method: 'POST',
+                url: baseUrl+"controladores/comprobaruser",
+                data: {nombre: nombre},
+                headers: { 'Content-Type': 'application/json' }
+            }).then(function(res) {
+
+                if(!!res.data) {
+                    // El usuario existe
+                    nombrePillado = false;
+                    $scope.correctoR.mostrar = false;
+                    $scope.alertaR.mostrar = true;
+                    $scope.alertaR.mensaje = "Otro entrenador tiene ese nombre de usuario.";
+
+                } else {
+                    // Todo correcto y el nombre no existe
+                    nombrePillado = true;
+                    $scope.alertaR.mostrar = false;
+                    $scope.correctoR.mostrar = true;
+                    $scope.correctoR.mensaje = " Puedes usar ese nombre de usuario."
+                }
+            });
+        }
+
+
+        $scope.LoginFn = function (datos) {
             console.log(datos);
             var userCorrecto = false;
             var passwordCorrecto = false;
@@ -129,19 +128,17 @@
 
                 // enviar
                 $http({
-                    method: 'POST',
-                    skipAuthorizacion: true,
-                    url: baseUrl + "controladores/login",
-                    data: datos,
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }).then(function(res) {
-                    console.log(res.data);
+                     method: 'POST',
+                     skipAuthorizacion: true,
+                     url: baseUrl + "controladores/login",
+                     data: datos,
+                     headers: { 'Content-Type': 'application/json' }
+                 }).then(function(res) {
+                     console.log(res.data);
 
-                    if (res.data.error && res.data.msg == 10) {
+                    if(res.data.error && res.data.msg == 10) {
                         store.set('token', res.data.token);
-                        if (res.data.save_name != "noob") {
+                        if(res.data.save_name != "noob") {
 
                             localStorage.setItem(res.data.save_name, res.data.save_data);
 
@@ -150,17 +147,21 @@
 
                         $scope.loginC.mensaje = "Todo correcto a jugar;)."
                         setTimeout(function() {
-                            window.location.href = baseUrl + "pokechat.html";
+                            window.location.href = baseUrl+"pokechat.html";
                         }, 1000);
-                    } else if (!res.data.error && res.data.msg == 11) {
+                    }
+
+                   else if(!res.data.error && res.data.msg == 11) {
                         $scope.alertaL.mostrar = true;
                         $scope.alertaL.mensaje = " La contraseña o el nombre de usuario no cohinciden.";
-                    } else if (!res.data.error && res.data.msg == 12) {
+                    }
+
+                    else if(!res.data.error && res.data.msg == 12) {
                         $scope.alertaL.mostrar = true;
                         $scope.alertaL.mensaje = " El usuario no existe ¡Registrate para la aventura!.";
                     }
 
-                });
+                 });
 
             }
 
@@ -169,7 +170,7 @@
 
         // El registro
 
-        $scope.RegisterFn = function(datos) {
+        $scope.RegisterFn = function (datos) {
             var userCorrecto = false;
             var passwordCorrecto = false;
 
@@ -191,66 +192,61 @@
             }
 
             // si está todo correcto marcar true y enviar
-            if (userCorrecto && passwordCorrecto) {
+            if (userCorrecto && passwordCorrecto && nombrePillado) {
                 // enviar
-                // hacer ajax
-                $http({
-                    method: 'POST',
-                    url: baseUrl + "controladores/registrar",
-                    data: datos,
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }).then(function(res) {
-                    if (!res.data.error) {
-                        // El usuario existe
-                        $scope.correctoR.mostrar = false;
-                        $scope.alertaR.mostrar = true;
-                        $scope.alertaR.mensaje = " Otro entrenador tiene ese nombre de usuario.";
+                       // hacer ajax
+            $http({
+                method: 'POST',
+                url: baseUrl+"controladores/registrar",
+                data: datos,
+                headers: { 'Content-Type': 'application/json' }
+            }).then(function(res) {
+                if(!res.data.error) {
+                    // El usuario existe
+                    nombrePillado = false;
+                    $scope.correctoR.mostrar = false;
+                    $scope.alertaR.mostrar = true;
+                    $scope.alertaR.mensaje = " Otro entrenador tiene ese nombre de usuario.";
 
-                    } else if (res.data.msg == 3 && res.data.error) {
-                        // Todo correcto y el nombre no existe
-                        $scope.alertaR.mostrar = false;
-                        $scope.correctoR.mostrar = true;
-                        $scope.correctoR.mensaje = " Ahora puedes empezar la aventura. ¡Serás redireccionado!";
+                } else if(res.data.msg == 3 && res.data.error) {
+                    // Todo correcto y el nombre no existe
+                    nombrePillado = true;
+                    $scope.alertaR.mostrar = false;
+                    $scope.correctoR.mostrar = true;
+                    $scope.correctoR.mensaje = " Ahora puedes empezar la aventura. ¡Serás redireccionado!";
 
-                        store.set('token', res.data.token);
+                    store.set('token', res.data.token);
 
-                        setTimeout(function() {
-                            window.location.href = baseUrl + "pokechat.html";
-                        }, 2000);
-                    }
+                    setTimeout(function() {
+                        window.location.href = baseUrl+"pokechat.html";
+                    }, 2000);
+                }
 
-                });
+            });
 
             }
+
+
+
         }
 
         // Efecto del switch del boton login
         var suitch = true;
         $scope.tipoRegistro = "Registrate ;)";
         $scope.botonRegistro = function() {
-            if (suitch) {
+            if(suitch) {
                 suitch = false;
-                $scope.mostrarLogin = {
-                    'opacity': '.3'
-                };
+                $scope.mostrarLogin = {'opacity':'.3'};
                 // Para el boton desactivado
                 $scope.desactivarBoton = "true";
-                $scope.mostrarRegistro = {
-                    'margin-top': '0px'
-                };
+                $scope.mostrarRegistro = {'margin-top': '0px'};
                 $scope.tipoRegistro = "Login";
             } else {
                 suitch = true;
-                $scope.mostrarLogin = {
-                    'opacity': '1'
-                };
+                $scope.mostrarLogin = {'opacity':'1'};
                 // para el boton activado
                 $scope.desactivarBoton = "false";
-                $scope.mostrarRegistro = {
-                    'margin-top': '-200px'
-                };
+                $scope.mostrarRegistro = {'margin-top': '-200px'};
                 $scope.tipoRegistro = "Registrate ;)";
             }
 
